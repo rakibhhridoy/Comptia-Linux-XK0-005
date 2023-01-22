e9eb51fc2cbfd2b3fbf47cb38fba5f92a8562c89
Rakib H. Hridoy <rakibhhridoy@yahoo.com>
2023-01-22 23:48:05 +0600

    +file

--- Section 2: Basic Linux Tasks/Section2.sh
+++ Section 2: Basic Linux Tasks/Section2.sh
@@ -5,3 +5,47 @@ man pages
 '
 which man
 man ls
+
+
+:'
+man pages sections
+'
+man 2 systemd
+
+
+
+:'man pages alternatives
+tldr = too long didnt read
+bro = bropages
+cheat = cheat pages
+'
+tldr ls
+bro ls
+cheat ls
+
+
+
+:'
+other helping commands
+'
+info ls
+info grep
+whatis ls
+
+
+
+
+:'
+location of man pages and documentation
+'
+ls /usr/share/man/
+tree /usr/share/doc/
+
+
+
+:'
+search commands
+'
+apropos boot
+
+
